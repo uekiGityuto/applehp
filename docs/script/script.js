0d8e916d22ad21b4faaaf10cd6624e0e805a6db0
yuto ueki <ganbaruzo000@gmail.com>
2019-04-12 03:56:16 +0900

    ハンバーガーメニューが表示時に画面が動くバグを修正

--- docs/script/script.js
+++ docs/script/script.js
@@ -6,7 +6,6 @@ $(function() {
 	$('.accbox').click(function() {
 		const size = window.innerWidth;
 		if(size < 670) {
-			//const $links = $(this).next('ul');
 			const $links = $(this).find("ul");
 			if($links.hasClass('open')) { 
 				$links.removeClass('open');
@@ -28,22 +27,20 @@ $(function() {
 		if($(".menu-icon").hasClass("open")) {
 			$(".menu-icon").removeClass("open");
 			$("header").css("background-color","#333333");
-			//$("#hidden-logo").css("visibility","visible");
 			$("#hidden-logo").css("opacity","1");
-			/*$("#hidden-logo").show();*/
 			$(".fa-times").removeClass("fa-times").addClass("fa-bars");
 			$(".hum-menu").slideUp();
-			$("body").css("overflow","visible");
+			/*$("body").css("overflow","visible");*/
+			$('html').removeClass('scroll-prevent')
 		}else {
 			$(".menu-icon").addClass("open");
 			$("header").css("background-color","#000");
-			//$("#hidden-logo").css("visibility","hidden");
 			$("#hidden-logo").css("opacity","0");
-			/*$("#hidden-logo").hide();*/
 			$(".fa-bars").removeClass("fa-bars").addClass("fa-times");
 			$(".hum-menu").slideDown();
 			/*背景がスクロールしないようにする*/
-			$("body").css("overflow","hidden");
+			/*$("body").css("overflow","hidden");*/
+			$('html').addClass('scroll-prevent')
 			$(".header-link").css("max-width", size);
 		}
 	})
@@ -54,37 +51,26 @@ $(function() {
 		if(!$("#head-search").hasClass("open")) {
 			$("#head-search").addClass("open");
 			$(".shopping-bag i").attr("id","hidden-logo");
-			//$("header").css("background-color","#000");
-			//$("article").css({"background-color": "rgba(0, 0, 0, 0.6)"});
-			//$("article").addClass("rgba");
 			$(".shade-wrapper").show();
 			$(".header-link .fa-shopping-bag").removeClass("fa-shopping-bag").addClass("fa-times");
-			//$(".search-hidden").hide();
-			//$(".search-hidden").css("visibility","hidden");
 			$(".search-hidden").css("opacity","0");
-			//$("#head-search").show();
 			$("#head-search").animate({width: "show"}, "100");
 			/*背景がスクロールしないようにする*/
-			$("body").css("overflow","hidden");
+			/*$("body").css("overflow","hidden");*/
+			$('html').addClass('scroll-prevent')
 		}
 	})
 })
 
 $(function() {
-/*	$("#hidden-logo").click(function() {*/
 	$("body").on("click","#hidden-logo",function() {
 		if($("#head-search").hasClass("open")) {
 			$("#head-search").removeClass("open");
-			//$("header").css("background-color","#333333");
-			//$("article").removeClass("rgba");
 			$(".shade-wrapper").hide();
 			$(".fa-times").removeClass("fa-times").addClass("fa-shopping-bag");
 			$(".shopping-contents").hide();
-			//$(".search-hidden").show();
-			//$(".search-hidden").css("visibility","visible");
 			$(".search-hidden").css("opacity","1");
 			$("#head-search").hide();
-			//$("#head-search").animate({width: "hide"}, "0");
 			$("body").css("overflow","visible");
 		}
 	})
@@ -93,38 +79,23 @@ $(function() {
 $(function() {
 	$(document).on('click touchend',function(e) {
 		if($("#head-search").hasClass("open")) {
-		//if (!$(e.target).closest('.shopping-contents').length) {
 			if (!$(e.target).closest('.hide-search').length) {
 				$("#head-search").removeClass("open");
-				//$("header").css("background-color","#333333");
-				//$("article").removeClass("rgba");
 				$(".shade-wrapper").hide();
 				$(".fa-times").removeClass("fa-times").addClass("fa-shopping-bag");
 				$(".shopping-contents").hide();
-				//$(".search-hidden").show();
-				//$(".search-hidden").css("visibility","visible");
 				$(".search-hidden").css("opacity","1");
 				$("#head-search").hide();
-				//$("#head-search").animate({width: "hide"}, "0");
 				$("body").css("overflow","visible");
 			}
 		}
 	})
 })
-
-
-/*
-$(function() {
-	$(".header-link .fa-shopping-bag").click(function() {
-		$(".shopping-contents").toggle();
-	})
-})
-*/
+/
 
 $(function() {
 	$(".header-link .shopping-bag").click(function() {
 		$(".shopping-contents").toggle();
-		/*$(".ueki").toggle();*/
 	})
 })
 
@@ -139,12 +110,4 @@ $(function() {
 })
 
 
-/*
-$(function() {
-	$(document).on('click touchend',function(e) {
-		if (!$(e.target).hasClass("hide-modal").length) {
-			$(".shopping-contents").hide();
-		}
-	})
-})
-*/
+
